93b9f48f5ade988e3922e119f36cda0413d9f417
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-16 20:17:59 +0300

    add authentication forms and validation schemas; update dependencies for form handling

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <AuthForm type="sign-up" />;
+};
+
+export default page;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// interface Props<T extends FieldValues>
+
+const AuthForm = ({ type, schema, defaultValues, onSubmit }: Props) => {
+  return <div>AuthForm -- {type}</div>;
+};
+
+export default AuthForm;
