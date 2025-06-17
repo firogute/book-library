6730686bd51d5324705f11cbc579b380bfc4843b
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-17 10:54:05 +0300

    refactor imageKit configuration and add API route for authentication parameters

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,11 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
-    imageKit: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    imageKit: {
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    },
   },
 };
 
