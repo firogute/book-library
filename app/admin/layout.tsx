728ceb7ed2b1e8ceb33bfd795a0c5609fa25fafe
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-22 18:08:44 +0300

    implement admin role verification in layout component

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,10 +4,22 @@ import React, { ReactNode } from "react";
 import "@/styles/admin.css";
 import SideBar from "@/components/admin/SideBar";
 import Header from "@/components/admin/Header";
+import { db } from "@/db/drizzle";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
   if (!session?.user?.id) redirect("/sign-in");
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user?.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  if (!isAdmin) redirect("/");
   return (
     <main className="flex  min-h-screen w-full flex-row">
       <SideBar session={session} />
